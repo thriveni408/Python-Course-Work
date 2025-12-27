ff9322edc7f0302c9253b94902ed979866ae82fd
thriveni408 <medarithriveni@gmail.com>
2025-12-27 13:50:44 +0530

    Exception_handling.py

--- class_works/Exception_Handling.py
+++ class_works/Exception_Handling.py
@@ -1,147 +1,103 @@
-#To handle ZeroDivisionError
-
 try:
     a=10/0
+    
+except ZeroDivisionError:
+    print("You can't divide a number with zero")
+else:
+    print("You have divided successfully")
+finally:
+    print("End of the try block")
+    
+print("rest of the code")
 
+
+
+#Name Error and TypeError
+
+try:
+    a=int([1,2,3])
+    
 except ZeroDivisionError:
     print("You can't divide a number with zero")
 
-else:
-    print("You have divided successfully")
-
-finally:
-    print("End of the try block")
-
-print("rest of the code")
-
-#To handle NameError
-
-try:
-    a=b/0
-
 except NameError:
-    print("You didn't define the variable")
-
-else:
-    print("You have divided successfully")
-
-finally:
-    print("End of the try block")
-
-print("rest of the code")
-
-
-##To handle TypeError
-
-try:
-    a=int([1,2,3])
+    print("You didnt define the variable")
 
 except TypeError:
     print("You can change this data type into other one")
-
 else:
     print("You have divided successfully")
-
 finally:
     print("End of the try block")
-
+    
 print("rest of the code")
 
-
-#To handle errors in a single line
+#CatchError in same line
 
 try:
     a=int([1,2,3])
-
+    
 except (ZeroDivisionError,NameError,TypeError,KeyError,IndexError) as e:
     print("Error Occured:",e)
+
+
 else:
     print("You have divided successfully")
-
 finally:
     print("End of the try block")
-
+    
 print("rest of the code")
 
-
-#To handle all kind of errors
+#Logic to handle all errors
 
 try:
     a=a+[1,2,3]
-
+    
 except Exception as e:
     print("Error Occured:",e)
+
+
 else:
     print("You have divided successfully")
-
 finally:
     print("End of the try block")
-
+    
 print("rest of the code")
 
+#RAISE EXCEPTION
 
-#raise keyword
 try:
-    a=input(input("Enter the number:"))
+    a=int(input("Enter the number:"))
     if a<0:
         raise Exception("Negative number not allowed")
+    
 except Exception as e:
-        print("Error Occured:",e)
+    print("Error Occured:",e)
+
+
 else:
     print("You have divided successfully")
-
 finally:
     print("End of the try block")
-
+    
 print("rest of the code")
 
+#TRY BLOCK WITH IN THE TRY BLOCK
 
-#nested try and except block    
-            
 try:
-    a=input(input("Enter the number:"))
+    a=int(input("Enter the number:"))
     try:
         a=b/10
     except Exception as e:
         print("Error Occured:",e)
+
+            
 except Exception as e:
     print("Error Occured:",e)
 
-
 else:
     print("You have divided successfully")
-
 finally:
     print("End of the try block")
-
-print("rest of the code")
-
-
     
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("rest of the code")
